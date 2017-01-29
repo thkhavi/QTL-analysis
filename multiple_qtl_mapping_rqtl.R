bec5d77138e5b538027dc608ba7f8401c5876ba7
thkhavi <thkhavi@tamu.edu>
2017-01-29 13:45:38 -0600

    Update multiple_qtl_mapping_rqtl.R

--- multiple_qtl_mapping_rqtl.R
+++ multiple_qtl_mapping_rqtl.R
@@ -1,51 +1,13 @@
 # Estimate QTL using multiple mapping with R/qtl package
 # (Much of this code originates from rqtl.org tutorials.)    
 
-# Change these file paths to reflect your system:
-input_file_directory = file.path("./")
-input_file_name_cross = file.path("./R07018xR07020_genetic_map_with_phenotypes.csv")
+# Load cross object
+load("./R07018xR07020")
 
-working_directory = getwd()
-if (!is.null(working_directory)) setwd(input_file_directory)
-
-# Generation interval of cross
-generation_interval = 6
-# Backcross interval of cross
-backcross_interval = 0
 # Phenotype: 
 phenotype = c("angle_leaf_3_avg_gh204A_2013_normalized")
 
 library(qtl)
-
-# Read in cross
-cross_inputcross = read.cross(format="csvr", 
-                              dir=input_file_directory,
-                              file=input_file_name_cross, 
-                              BC.gen=backcross_interval, 
-                              F.gen=generation_interval,
-                              genotypes=c("AA","AB","BB","notAA","notBB"))
-
-# If the cross type is considered a RIL keep the next line, if not comment out with "#"
-cross_inputcross = convert2riself(cross_inputcross)
-
-################################################################################
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
-# multiple-QTL-model traversal as currently implemented (10/2014) is unable to handle > 1400 markers
-# If your genetic map is made of more than 1400 markers, you may need to thin out markers:
-# Choose the distance (in cM) to thin out
-# marker_distance = 0.5
-# If you don't want to thin markers, then comment out with "#"
-# cross_inputcross_map = pull.map(cross_inputcross) 
-# markers2keep = lapply(cross_inputcross_map, pickMarkerSubset, min.distance=marker_distance) 
-# cross_sub = pull.markers(cross_inputcross, unlist(markers2keep))
-# cross_inputcross = cross_sub
-#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#
-################################################################################
-
-cross_inputcross = calc.genoprob(cross_inputcross,
-                                 map.function = "haldane")
-cross_inputcross = sim.geno(cross_inputcross, 
-                            map.function = "haldane")
 
 ################################################################################
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>#
