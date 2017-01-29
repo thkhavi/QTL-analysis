09e6805d26b96b39edaa4011bdd684be0c47240f
thkhavi <thkhavi@tamu.edu>
2017-01-29 13:47:13 -0600

    Update single_qtl_mapping_rqtl.R

--- single_qtl_mapping_rqtl.R
+++ single_qtl_mapping_rqtl.R
@@ -1,17 +1,8 @@
 # Estimate QTL using interval mapping with R/qtl package
 # (Much of this code originates from rqtl.org tutorials.)              
 
-# Change these file paths to reflect your system:
-input_file_directory = file.path("./")
-input_file_name_cross = file.path("./R07018xR07020_genetic_map_with_phenotypes.csv")
-
-working_directory = getwd()
-if (!is.null(working_directory)) setwd(input_file_directory)
-
-# Generation interval of cross
-generation_interval = 6
-# Backcross interval of cross
-backcross_interval = 0
+# Load cross object
+load("./R07018xR07020")
 
 # Available phenotypes:
 phenotype_list = c("angle_leaf_3_avg_gh204A_2013_normalized",
@@ -28,22 +19,6 @@ number_permutations = 10
 alpha_value = 0.05
 
 library(qtl)
-
-# Read in cross
-cross_inputcross = read.cross(format="csvr", 
-                               dir=input_file_directory,
-                               file=input_file_name_cross, 
-                               BC.gen=backcross_interval, 
-                               F.gen=generation_interval,
-                               genotypes=c("AA","AB","BB","notAA","notBB"))
-
-# If the cross type is considered a RIL keep the next line, if not comment out with "#"
-# cross_inputcross = convert2riself(cross_inputcross)
-
-cross_inputcross = calc.genoprob(cross_inputcross,
-                                 map.function = "haldane")
-cross_inputcross = sim.geno(cross_inputcross, 
-                            map.function = "haldane")
 
 # For each phenotype:
 for (phenotype in 1:(length(phenotype_list))) {
