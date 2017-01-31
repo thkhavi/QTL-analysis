9f140b3844922e4c08cb8902c4394bbb358a5c8d
thkhavi <thkhavi@tamu.edu>
2017-01-31 08:53:33 -0600

    Update single_qtl_mapping_rqtl.R

--- single_qtl_mapping_rqtl.R
+++ single_qtl_mapping_rqtl.R
@@ -2,21 +2,18 @@
 # (Much of this code originates from rqtl.org tutorials.)              
 
 # Load cross object
-load("./R07018xR07020")
+load("./R07018_R07020_2017-01-30")
+
+# Cross name
+input_file_name_cross = "R07018_R07020_2017-01-30"
 
 # Available phenotypes:
 phenotype_list = c("angle_leaf_3_avg_gh204A_2013_normalized",
-                   "angle_leaf_4_avg_gh204A_2013_normalized",
-                   "angle_leaf_3_avg_csfield_2014_rep1_normalized",
-                   "angle_leaf_4_avg_csfield_2014_rep1_normalized",
-                   "angle_leaf_5_avg_csfield_2014_rep1_normalized",
-                   "angle_leaf_3_avg_csfield_2014_rep2_normalized",
-                   "angle_leaf_4_avg_csfield_2014_rep2_normalized",
-                   "angle_leaf_5_avg_csfield_2014_rep2_normalized")
+                   "angle_leaf_4_avg_gh204A_2013_normalized")
 
-# Permutations (R/qtl developers expect at least 1000) & alpha
-number_permutations = 10
 alpha_value = 0.05
+number_permutations = 25000
+main_permutation_threshold = 3.205457
 
 library(qtl)
 
@@ -32,86 +29,63 @@ for (phenotype in 1:(length(phenotype_list))) {
   print(phenotype_list[phenotype])
   cat("\t interval mapping \n")
   scanone_inputcross = scanone(cross = cross_inputcross,
-                                method = c("em"),
-                                pheno.col = phenotype_list[phenotype])
-  
-  # Permutations
-  cat("\t permutations \n")
-  scanone_inputcross_perm = scanone(cross = cross_inputcross,
-                                     method=c("em"),
-                                     pheno.col=phenotype_list[phenotype],
-                                     n.perm=number_permutations)
-  
-  cat("\t", summary(scanone_inputcross_perm, alpha = alpha_value), "\n")
-  print(proc.time() - ptm)
+                               method = c("em"),
+                               pheno.col = phenotype_list[phenotype])
   
   cat("\t creating plots \n")
   scanone_plot_name = paste("scanone", phenotype_list[phenotype], sep = " - ")
   scanone_file_plot_name = paste0("scanone", "-", phenotype_list[phenotype], ".png")
   
   # Set y-limit on plot to be above the largest lod (either from permutations or single-QTL analysis)
-  ylimit = max(summary(scanone_inputcross_perm, alpha = alpha_value), max(scanone_inputcross$lod))
+  ylimit = max(main_permutation_threshold, max(scanone_inputcross$lod))
   
   png(scanone_file_plot_name, width=1600, height=800, res=200)
-  plot(scanone_inputcross, col = "blue", 
+  plot(scanone_inputcross, col = "green", 
        lty=1, 
        ylim=c(0, ylimit), 
        main=scanone_plot_name)
-  add.threshold(scanone_inputcross, 
-                perms=scanone_inputcross_perm, 
-                alpha=alpha_value, 
-                col="red", 
-                lty=2)
   dev.off()
   
   # Additive and dominance (if not RIL) effect plot
   effect_plot_name = paste("effect", phenotype_list[phenotype], sep = " - ")
   effect_file_plot_name = paste0("effect","-", phenotype_list[phenotype], ".png")
-
+  
   png(effect_file_plot_name, width=1600, height=800, res=200)
-  effectscan(cross_inputcross, 
-             pheno.col=phenotype_list[phenotype], 
-             get.se=FALSE, 
-             draw=TRUE, 
-             main=effect_plot_name)
-  dev.off() 
+  effectscan_values = effectscan(cross_inputcross, 
+                                 pheno.col=phenotype_list[phenotype], 
+                                 get.se=TRUE, 
+                                 draw=TRUE, 
+                                 main=effect_plot_name)
+  dev.off()
   
   # Construct list of significant markers (alpha=0.05)
-  significant_markers_file_name = paste0("signficant_markers_", phenotype_list[phenotype],".csv")
+  significant_markers_file_name = paste0("significant_markers_", phenotype_list[phenotype],".csv")
   sink(file=significant_markers_file_name, append = FALSE)
   cat("Significant markers for \n",
       "\t phenotype:", phenotype_list[phenotype], "\n",
       "\t cross object:", input_file_name_cross, "\n",
       "\t alpha-value:", alpha_value, "\n",
       "\t permutations:", number_permutations, "\n", 
-      "\t lod threshold:", summary(scanone_inputcross_perm, alpha = alpha_value), "\n\n")
-  cat("chr_position(bp)_variant", ",", "position(cM)",	",", "lod","\n")
+      "\t lod threshold:", main_permutation_threshold, "\n\n")
+  cat("chr_position(bp)_variant", ",", "position(cM)",	",", "lod", ",", "effect(a)", ",", "SE","\n")
   sink()
   for (marker in 1:(length(scanone_inputcross$lod))) {
-    if (scanone_inputcross$lod[marker] >= summary(scanone_inputcross_perm, alpha = alpha_value)) {
+    if (scanone_inputcross$lod[marker] >= main_permutation_threshold) {
       sink(file = significant_markers_file_name, append = TRUE)
-      cat(rownames(scanone_inputcross)[marker], ",", scanone_inputcross$pos[marker], ",", scanone_inputcross$lod[marker],"\n")
+      cat(rownames(scanone_inputcross)[marker], ",",
+          scanone_inputcross$pos[marker], ",",
+          scanone_inputcross$lod[marker], ",",
+          effectscan_values$a[marker], ",",
+          effectscan_values$se.a[marker],"\n")
       sink()
     }
   }
-  
-  # Construct folder containing phenotype x genotype plots of significant markers (alpha=0.05)
-  # Create p x g directory
-  pxg_directory = "pxg_plots"
-  dir.create(pxg_directory)
-  setwd(file.path(pxg_directory))
-  for (marker in 1:(length(scanone_inputcross$lod))) {
-    if (scanone_inputcross$lod[marker] >= summary(scanone_inputcross_perm, alpha = alpha_value)) {
-      PXG_name = paste0("pxg_", rownames(scanone_inputcross)[marker], ".png")
-      png(PXG_name, width=1600, height=800, res=200)
-      plotPXG(cross_inputcross, 
-              rownames(scanone_inputcross)[marker],
-              pheno.col=phenotype_list[phenotype],
-              jitter=1,
-              infer=TRUE,
-              main=rownames(scanone_inputcross)[marker])
-      dev.off()
-    }
+  # Save the lod intervals. Note that this assumes 10 chromosomes.
+  sink(file=significant_markers_file_name, append = TRUE)
+  cat("\nLOD-2 intervals for all chromosomes:\n")
+  for (k in 1:10) {
+    print(lodint(scanone_inputcross, drop=2, chr=k, expandtomarkers=TRUE))
   }
-  setwd("../../")
+  sink()
+  setwd("../")
 }
